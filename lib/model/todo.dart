9aa9fbcdaedeb405ee16b9125a49164ec1ac267b
hananiiii <h.lachoub@esi-sba>
2023-08-24 16:27:14 +0100

    pushing

---
+++ lib/model/todo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList(){
+    return [
+      ToDo(id: '01', todoText: 'Morning Excercise' , isDone: true),
+       ToDo(id: '02', todoText: 'Buy Groceries' , ),
+        ToDo(id: '03', todoText: 'Clean my room' , ),
+         ToDo(id: '04', todoText: 'Prepare Lunch' , ),
+          ToDo(id: '05', todoText: 'Check mail' , isDone: true),
+           ToDo(id: '06', todoText: 'Take a nap' , isDone: true),
+    ];
+  }
+}
